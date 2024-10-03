58811b621196626690d22cda5a0e5283c1139447
geysianelopes <geysianelopes@hotmail.com>
2024-10-03 18:30:06 -0400

    add question 2 and 3

--- question-3/question-3.js
+++ question-3/question-3.js
@@ -1,3 +1,33 @@
 // Student Name: Geysiane Lopes Bezerra
 // StudentID: 101403935
-c
+const fs = require('fs');
+const path = require('path');
+
+const logsDir = path.join(__dirname, "Logs");
+
+
+const createLogs = () => {
+    if (!fs.existsSync(logsDir))
+        fs.mkdirSync(logsDir);
+
+    for (let i = 0; i < 10; i++) {
+        const fileName = `log${i}.txt`;
+        const filePath = path.join(logsDir, fileName);
+        fs.writeFileSync(filePath, `This is log file number ${i}`);
+        console.log(fileName);
+    }
+}
+
+const deleteLogs = () => {
+    if (fs.existsSync(logsDir)) {
+        let files = fs.readdirSync(logsDir);
+        for (let file of files) {
+            const fullFilePath = path.join(logsDir, file);
+            fs.unlinkSync(fullFilePath);
+            console.log(`delete files...${file}`);
+        }
+    }
+}
+
+createLogs();
+deleteLogs();
